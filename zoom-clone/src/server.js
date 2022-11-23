a0ba940514c9332d2f631ea445ab3837b0be606f
k0102575 <k0102575@gmail.com>
2022-11-23 21:51:47 +0900

    feat: web socket server setting

--- zoom-clone/src/server.js
+++ zoom-clone/src/server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import ejs from 'ejs';
+import http from 'http';
+import WebSocket from 'ws';
 
 // variables
 const app = express();
@@ -6,16 +9,26 @@ const PORT_NUMBER = 3000;
 
 // view template ejs Setting
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.engine('html', ejs.renderFile);
 app.set('views', __dirname + '/views');
 
 // Route Setting
 app.get('/', (req, res) => res.render('index'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 // static file Setting
 app.use('/public', express.static(__dirname + '/public'));
 
-// App start
-app.listen(PORT_NUMBER, '', () => {
+// callback
+const handleListen = () => {
   console.log(`start server ===> port: ${PORT_NUMBER}`);
-});
+};
+
+// express server start
+// app.listen(PORT_NUMBER, '', handleListen);
+
+// Web socket server start
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(PORT_NUMBER, handleListen);
