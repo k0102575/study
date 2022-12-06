870e40785011be14e5fa95f1426284664cc13e7b
k0102575 <k0102575@gmail.com>
2022-12-06 21:51:10 +0900

    feat: #2 SocketIo

--- zoom-clone/src/public/js/app.js
+++ zoom-clone/src/public/js/app.js
@@ -1,46 +1,90 @@
-const messageList = document.querySelector('ul');
-const nicknameForm = document.querySelector('#nicknameForm');
-const messageForm = document.querySelector('#messageForm');
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
+const room = document.getElementById('room');
+let roomName;
 
-const makeMessage = (type, payload) => {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+// IIFE
+(function () {
+  room.hidden = true;
+})();
+
+// message handler
+const addMessage = (msg) => {
+  const ul = room.querySelector('ul');
+  const li = document.createElement('li');
+
+  console.log(msg);
+  li.innerText = msg;
+  ul.appendChild(li);
 };
 
-// Web socket connection event
-socket.addEventListener('open', () => {
-  console.log('서버와 연결 되었습니다. ✅');
-});
-
-// Web socket receive message event
-socket.addEventListener('message', (message) => {
-  const li = document.createElement('li');
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-// Web socket close event
-socket.addEventListener('close', () => {
-  console.log('서버와 연결이 끊겼습니다. ❌');
-});
-
-// nickname Form submit Event
-nicknameForm.addEventListener('submit', (event) => {
+// welcome form submit event handler
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const input = nicknameForm.querySelector('input');
-  socket.send(makeMessage('nickname', input.value));
-});
 
-// Message Form submit event
-messageForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const input = messageForm.querySelector('input');
+  const input = form.querySelector('input');
 
-  socket.send(makeMessage('new_message', input.value));
-  const li = document.createElement('li');
-  li.innerText = `You: ${input.value}`;
-  messageList.append(li);
+  socket.emit('enter_room', input.value, () => {
+    welcome.hidden = true;
+    room.hidden = false;
+
+    const h3 = room.querySelector('h3');
+
+    h3.innerText = `${roomName}`;
+
+    // 메시지 보내기 폼 submit 이벤트
+    const msgForm = room.querySelector('#msg');
+    msgForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = room.querySelector('#msg input');
+      const value = input.value;
+      socket.emit('new_message', input.value, roomName, () => {
+        addMessage(`나 : ${value}`);
+      });
+      input.value = '';
+    });
+
+    // 닉네임 폼 submit 이벤트
+    const nicknameForm = room.querySelector('#nickname');
+    nicknameForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = room.querySelector('#nickname input');
+      socket.emit('nickname', input.value);
+    });
+  });
+
+  roomName = input.value;
 
   input.value = '';
+});
+
+socket.on('welcome', (user, userCount) => {
+  const h3 = room.querySelector('h3');
+  h3.innerText = `방 ${roomName} 사람수 ${userCount}`;
+
+  addMessage(`${user} joined!`);
+});
+
+socket.on('bye', (user, userCount) => {
+  const h3 = room.querySelector('h3');
+  h3.innerText = `방 ${roomName} 사람수 ${userCount}`;
+
+  addMessage(`${user} left!`);
+});
+
+socket.on('new_message', addMessage);
+
+socket.on('room_change', (rooms) => {
+  const roomList = welcome.querySelector('ul');
+  roomList.innerHTML = '';
+
+  if (rooms.length) {
+    rooms.forEach((room) => {
+      const li = document.createElement('li');
+      li.innerText = room;
+      roomList.appendChild(li);
+    });
+    return;
+  }
 });
--- zoom-clone/src/server.js
+++ zoom-clone/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import ejs from 'ejs';
 import http from 'http';
-import WebSocket from 'ws';
+// import WebSocket from 'ws';
+import { Server } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 
 // variables
 const app = express();
@@ -19,45 +21,117 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('index'));
 app.get('/*', (req, res) => res.redirect('/'));
 
-// Http server start
-const server = http.createServer(app);
-server.listen(PORT_NUMBER, () => {
+// server start
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ['https://admin.socket.io'],
+    credentials: true,
+  },
+});
+
+instrument(io, {
+  auth: false,
+});
+
+httpServer.listen(PORT_NUMBER, () => {
   console.log(`start server ===> port: ${PORT_NUMBER}`);
 });
 
-// Web socket server start
-const wss = new WebSocket.Server({ server });
+const publicRooms = () => {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = io;
 
-// fake database
-const sockets = [];
+  const publicRooms = [];
 
-// Web socket event hander
-wss.on('connection', (socket) => {
-  sockets.push(socket);
-  socket['nickname'] = 'Anon';
-
-  // Web socket connection event
-  console.log('클라이언트와 연결 되었습니다. ✅');
-
-  // Web socket close event
-  socket.on('close', () => {
-    console.log('클라이언트와 연결이 끊겼습니다. ❌');
-  });
-
-  // Web socket receive message event
-  socket.on('message', (message) => {
-    const { type, payload } = JSON.parse(message);
-
-    switch (type) {
-      case 'new_message':
-        sockets.forEach((_socket) => {
-          _socket.send(`${socket.nickname} : ${payload}`);
-        });
-        break;
-
-      case 'nickname':
-        socket['nickname'] = payload;
-        break;
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
     }
   });
+
+  return publicRooms;
+};
+
+const countRoom = (roomName) => {
+  return io.sockets.adapter.rooms.get(roomName)?.size;
+};
+
+io.on('connection', (socket) => {
+  socket['nickname'] = 'Anon';
+
+  // onAny event
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+
+  // enter_room event
+  socket.on('enter_room', (roomName, done) => {
+    socket.join(roomName);
+    done();
+
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
+
+    io.sockets.emit('room_change', publicRooms());
+  });
+
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1);
+    });
+  });
+
+  socket.on('disconnect', () => {
+    io.sockets.emit('room_change', publicRooms());
+  });
+
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+    done();
+  });
+
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
+
+/*
+  Web Socket
+*/
+// // Web socket server start
+// const wss = new WebSocket.Server({ server });
+
+// // fake database
+// const sockets = [];
+
+// // Web socket event hander
+// wss.on('connection', (socket) => {
+//   sockets.push(socket);
+//   socket['nickname'] = 'Anon';
+
+//   // Web socket connection event
+//   console.log('클라이언트와 연결 되었습니다. ✅');
+
+//   // Web socket close event
+//   socket.on('close', () => {
+//     console.log('클라이언트와 연결이 끊겼습니다. ❌');
+//   });
+
+//   // Web socket receive message event
+//   socket.on('message', (message) => {
+//     const { type, payload } = JSON.parse(message);
+
+//     switch (type) {
+//       case 'new_message':
+//         sockets.forEach((_socket) => {
+//           _socket.send(`${socket.nickname} : ${payload}`);
+//         });
+//         break;
+
+//       case 'nickname':
+//         socket['nickname'] = payload;
+//         break;
+//     }
+//   });
+// });
